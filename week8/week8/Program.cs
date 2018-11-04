af02bdfa2687502812f999d2ccf9a801d5b50c7f
nantikan <60030049@kmitl.ac.th>
2018-11-04 21:43:15 +0700

    add

--- week8/week8/Program.cs
+++ week8/week8/Program.cs
@@ -10,7 +10,18 @@ namespace week8
     {
         static void Main(string[] args)
         {
-            
+            string[] month2 = new string[12];
+            string[] month3 = new string[12];
+            string[] month1 = { "January", "February", "March", "April", "May", "June",
+                               "July", "August", "September", "October", "November", "Desember" };
+            string[] mc1 = Array.FindAll(month1, m1 => m1.EndsWith("ry", StringComparison.Ordinal));
+            string[] mc2 = Array.FindAll(month1, m2 => m2.EndsWith("ber", StringComparison.Ordinal));
+            month3 = mc1;
+            month2 = mc2;
+            Console.WriteLine("========End ber=========");
+            Console.WriteLine(string.Join(",",month2));
+            Console.WriteLine("========End ry=========");
+            Console.WriteLine(string.Join(",", month3));
         }
     }
 }
