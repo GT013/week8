f1d9f364932b95f4d2259c96e2336d6a2a58a1d8
nantikan <60030049@kmitl.ac.th>
2018-11-04 22:58:15 +0700

    add

--- week8/week8/Program.cs
+++ week8/week8/Program.cs
@@ -10,30 +10,17 @@ namespace week8
     {
         static void Main(string[] args)
         {
-            int[][] jagged = new int[7][];
-            int count = 0;
-            for (int row = 0; row < jagged.GetLength(0); ++row)
+
+            string[][] md1 = new string[2][];
+            md1[0] = new string[] { "Sunday", "Monday", "Tyesday", "Wendsday", "Thursday", "Friday", "Saterday" };
+            md1[1] = new string[] { "Jan", "Feb", "Mar", "April", "May", "June",
+                "July", "August", "Sep", "Oc", "November", "December" };
+
+            for (int i = 0; i < md1.GetLength(0); ++i)
             {
-                Console.Write("\nRow {0}:", row);
-                jagged[row] = new int[row + 1];
-                for (int index = 0; index < row + 1; ++index)
-                {
-                    ++count;
-                    jagged[row][index] = count;
-                    Console.Write(" {0}", count);
-                }
+               
+
             }
-            Console.WriteLine("\n\nTotals");
-            for (int row = 0; row < jagged.GetLength(0); ++row)
-            {
-                int total = 0;
-                for (int index = 0; index < jagged[row].GetLength(0); ++index)
-                {
-                    total += jagged[row][index];
-                }
-                Console.Write("\nRow {0}: {1}", row, total);
-            }
-            Console.ReadLine();
 
         }
     }
