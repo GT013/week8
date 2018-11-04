b316001be70793766bdcb8cec51547634d5affeb
nantikan <60030049@kmitl.ac.th>
2018-11-04 20:11:37 +0700

    add

--- week8/week8/Program.cs
+++ week8/week8/Program.cs
@@ -10,36 +10,16 @@ namespace week8
     {
         static void Main(string[] args)
         {
-            string[] array1 = { "cat", "dog", "carrot", "bird" };
+            string[] month = { "January", "February", "March", "April", "May", "June",
+                               "July", "August", "September", "October", "November", "Desember" };
 
-            //
-            // ค้นหาสมาชิกตัวแรกที่มีค่าตามกำหนด 
-            //
-            string value1 = Array.Find(array1,
-              element => element.StartsWith("car",
-              StringComparison.Ordinal));
-
-            string value2 = Array.Find(array1,
-              element => element.StartsWith("fish",
-              StringComparison.Ordinal));
-
-            //
-            // ค้นหาสมาชิกตัวแรกที่มีความยาว string ตามกำหนด
-            //
-            string value3 = Array.Find(array1,
-                element => element.Length == 3);
-
-            //
-            // ค้นหา string ที่มีความยาวไม่เกินค่าที่กำหนด
-            //
-            string[] array2 = Array.FindAll(array1,
-                element => element.Length <= 4);
+            string value1 = Array.Find(month, m => m.Length<=3);
+            string value2 = Array.Find(month, m => m.Length > 10);
+            string[] value3 = Array.FindAll(month, m => m.StartsWith("J", StringComparison.Ordinal));
 
             Console.WriteLine(value1);
             Console.WriteLine(value2);
-            Console.WriteLine(value3);
-            Console.WriteLine(string.Join(",", array2));
-            Console.ReadLine();
+            Console.WriteLine(string.Join(",", value3));
 
         }
     }
