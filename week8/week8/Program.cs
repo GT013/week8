274c3494b5f4847051f2694c68b49327e352373d
nantikan <60030049@kmitl.ac.th>
2018-11-04 19:22:36 +0700

    add

--- week8/week8/Program.cs
+++ week8/week8/Program.cs
@@ -10,29 +10,39 @@ namespace week8
     {
         static void Main(string[] args)
         {
+            int d, m;
+            int s, n = 0;
+            string[] day = { "sunday", "monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sat" };
 
-            double[] gps = new double[8];
-            float g;
-            float s = 0;
-            float cr = 0;
-            int c;
-           
+            int[] monthday = new int[12];
+            monthday[0] = 0;
+            monthday[1] = 3;
+            monthday[2] = 3;
+            monthday[3] = 6;
+            monthday[4] = 1;
+            monthday[5] = 4;
+            monthday[6] = 6;
+            monthday[7] = 2;
+            monthday[8] = 5;
+            monthday[9] = 0;
+            monthday[10] = 3;
+            monthday[11] = 5;
 
-            for (int i = 0; i < 8; i++)
+            Console.Write("Date = ");
+            d = int.Parse(Console.ReadLine());
+            Console.Write("Month = ");
+            m = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= 12; i++)
             {
+                if (m == (i+1))
+                {
+                    s = d + monthday[i]-1;
 
-                Console.Write("Credit = ");
-                c = int.Parse(Console.ReadLine());
-                cr += c;
-                Console.Write("GPS = ");
-                g = float.Parse(Console.ReadLine());
-                s = s + g*c;
+                    n = s % 7;
+                    Console.WriteLine("{0}/{1}/2017 is {2}", d, m, day[n]);
+                }
+
             }
-
-            Console.WriteLine("GPA = {0:F2} ", s / cr);
-
-
-
 
         }
     }
