c3f56958dadd998a822f19160e8edbbe4ef335dd
nantikan <60030049@kmitl.ac.th>
2018-11-05 23:17:13 +0700

    add

--- week8/week8/Program.cs
+++ week8/week8/Program.cs
@@ -12,19 +12,19 @@ namespace week8
         static void Main(string[] args)
         {
             ArrayList list1 = new ArrayList();
-            list1.Add(1);
-            list1.Add(2);
-            list1.Add(3);
-            list1.Add(4);
-            list1.Add(5);
+            list1.Add("Nantikan");
+            list1.Add("Hello");
+            list1.Add("Hi");
+            list1.Add("Yeah");
+            list1.Add("Ohhhhhh");
             Console.WriteLine("======before======");
-            foreach (int n in list1)
+            foreach (string n in list1)
             {
                 Console.WriteLine(n);
             }
-            list1.RemoveAt(3);
+            list1.Remove("Hi");
             Console.WriteLine("======After=======");
-            foreach(int n in list1)
+            foreach(string n in list1)
             {
                 Console.WriteLine(n);
             }
