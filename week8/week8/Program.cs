70bd6fea14aa80fc429328bd382d015feb6e3a6a
nantikan <60030049@kmitl.ac.th>
2018-11-05 21:24:30 +0700

    add

--- week8/week8/Program.cs
+++ week8/week8/Program.cs
@@ -13,18 +13,18 @@ namespace week8
            
             Hashtable name = new Hashtable();
            
-            name.Add(60030049, "Nantikan Tippawan");
-            name.Add(60030007, "Kasina");
-            name.Add(60030037, "Thanapol");
-            name.Add(60030029, "Natthapat");
-            name.Add(60030014, "Jarudet");
-            name.Add(60030031, "Thanakamon");
-            name.Add(60030098, "Sittinon");
-            name.Add(60030015, "Jinnapat");
-            name.Add(60030112, "Avirut");
-            name.Add(60030016, "Jirawat");
-            name.Add(60030089, "Sarun");
-            name.Add(60030041, "Thanakorn");
+            name.Add("60030049", "Nantikan Tippawan");
+            name.Add("60030007", "Kasina");
+            name.Add("60030037", "Thanapol");
+            name.Add("60030029", "Natthapat");
+            name.Add("60030014", "Jarudet");
+            name.Add("60030031", "Thanakamon");
+            name.Add("60030098", "Sittinon");
+            name.Add("60030015", "Jinnapat");
+            name.Add("60030112", "Avirut");
+            name.Add("60030016", "Jirawat");
+            name.Add("60030089", "Sarun");
+            name.Add("60030041", "Thanakorn");
 
 
             Console.WriteLine("---- Computer Class ----");
@@ -34,7 +34,7 @@ namespace week8
             }
             Console.WriteLine("---------------ID Student-----------");
 
-            Console.WriteLine(" " + name.ContainsKey(Console.ReadLine()));
+            Console.WriteLine(" "+name.ContainsKey(Console.ReadLine()));
 
         }
     }
