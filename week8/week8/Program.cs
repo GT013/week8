3d226da86c72f4f2181f4f6d4a1d7fda34859802
nantikan <60030049@kmitl.ac.th>
2018-11-05 21:50:43 +0700

    add

--- week8/week8/Program.cs
+++ week8/week8/Program.cs
@@ -10,38 +10,8 @@ namespace week8
     {
         static void Main(string[] args)
         {
-            string t;
-            Dictionary<string, string> d1 = new Dictionary<string, string>();
-
-            Console.Write("Type File = ");
-            t = Console.ReadLine();
-            d1.Add("txt", "Notepad.exe");
-            d1.Add("bmp", "paint.exe");
-            d1.Add("rtf", "wordpad.exe");
-            d1.Add("pdf", "acrobat.exe");
-            
-          
-            if (t=="pdf")
-            {
-                Console.WriteLine(d1["pdf"]);
-            }
-            else if (t=="txt")
-            {
-                Console.WriteLine(d1["txt"]);
-            }
-            else if (t=="bmp")
-            {
-                Console.WriteLine(d1["bmp"]);
-            }
-            else if (t == "rtf")
-            {
-                Console.WriteLine(d1["rtf"]);
-            }
-            else
-            {
-                Console.WriteLine("No Program!!!");
-            }
-
+           
         }
+
     }
 }
