6b383e8e2d78bcc6c433c8f8280209962a5590ed
nantikan <60030049@kmitl.ac.th>
2018-11-05 22:10:25 +0700

    add

--- week8/week8/Program.cs
+++ week8/week8/Program.cs
@@ -10,8 +10,35 @@ namespace week8
     {
         static void Main(string[] args)
         {
-           
-        }
+            string[][] land = new string[6][];
+            land[0] = Asa;
+            Console.WriteLine("=========Asia=========");
+            Console.WriteLine();
 
+        }
+        static string[] Asa = new string[]
+        {
+            "Cambodia","Qatar","South Korea","North Korea","Kazakhstan","Kyrgyzstan","Japan","Timor-Leste"
+        };
+        static string[] Os = new string[]
+        {
+            "Kiribati","Samoa","Tonga","Tuvalu","Nauru","New Zealand","Papua New Guinea","Fiji","Palau","Micronesia"
+        };
+        static string[] Ur = new string[]
+        {
+            "Greece","Kosovo","Croatia","Czechia","Vatican City State","Norway"
+        };
+        static string[] Af = new string[]
+        {
+            "Ghana","Gabon","Guinea-Bissau","Djibouti"
+        };
+        static string[] AmNo = new string[]
+        {
+            "Guatemala","Grenada","Saint Vincent and the Grenadines"
+        };
+        static string[] AmSo = new string[]
+        {
+            "Guyana","Colombia","Chile","Suriname"
+        };
     }
 }
