87f6547922ce6042a94bda9fc4b433bad87a0ddf
nantikan <60030049@kmitl.ac.th>
2018-11-05 23:54:33 +0700

    add

--- week8/week8/Program.cs
+++ week8/week8/Program.cs
@@ -12,22 +12,28 @@ namespace week8
         static void Main(string[] args)
         {
             ArrayList list1 = new ArrayList();
-            list1.Add(1);
-            list1.Add(2);
-            list1.Add(3);
-            list1.Add(4);
-            list1.Add(5);
-            Console.WriteLine("======before======");
-            foreach (int n in list1)
+            list1.Add("1");
+            list1.Add("2");
+            list1.Add("3");
+            list1.Add("4");
+            list1.Add("5");
+            list1.Add("6");
+            list1.Add("7");
+           // Console.WriteLine("======before======");
+            foreach (string n in list1)
             {
                 Console.WriteLine(n);
             }
-            list1.RemoveAt(3);
-            Console.WriteLine("======After=======");
-            foreach(int n in list1)
-            {
-                Console.WriteLine(n);
-            }
+            Console.WriteLine("Indexof : {0}",list1.IndexOf("5",3));
+
+
+
+           // Console.WriteLine("======After=======");
+
+            //foreach(string n in list1)
+            //{
+            //    Console.WriteLine(n);
+            //}
 
         }
     }
